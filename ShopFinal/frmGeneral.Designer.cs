73b28f1f638db351771887231d284cae75dadb1d
AriOha <ariel@ohayon.co.il>
2021-07-02 17:00:52 +0300

    all tabs w/o func, updated fill functions(not done)

--- ShopFinal/frmGeneral.Designer.cs
+++ ShopFinal/frmGeneral.Designer.cs
@@ -30,7 +30,7 @@ namespace ShopFinal
         private void InitializeComponent()
         {
             this.lstProducts = new System.Windows.Forms.ListBox();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabMain = new System.Windows.Forms.TabControl();
             this.tabProducts = new System.Windows.Forms.TabPage();
             this.btnAddProd = new System.Windows.Forms.Button();
             this.btnRemoveProd = new System.Windows.Forms.Button();
@@ -46,29 +46,42 @@ namespace ShopFinal
             this.cboEditSupp = new System.Windows.Forms.ComboBox();
             this.txtEditProd = new System.Windows.Forms.TextBox();
             this.lblProdTitle = new System.Windows.Forms.Label();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabControl1.SuspendLayout();
+            this.tabSuppliers = new System.Windows.Forms.TabPage();
+            this.lstvProducts = new System.Windows.Forms.ListView();
+            this.txtSuppliers = new System.Windows.Forms.Label();
+            this.lstSuppliers = new System.Windows.Forms.ListBox();
+            this.tabCustomers = new System.Windows.Forms.TabPage();
+            this.tabOrders = new System.Windows.Forms.TabPage();
+            this.tabCreate = new System.Windows.Forms.TabPage();
+            this.tabReport = new System.Windows.Forms.TabPage();
+            this.tabMain.SuspendLayout();
             this.tabProducts.SuspendLayout();
+            this.tabSuppliers.SuspendLayout();
             this.SuspendLayout();
             // 
             // lstProducts
             // 
             this.lstProducts.FormattingEnabled = true;
-            this.lstProducts.Location = new System.Drawing.Point(20, 75);
+            this.lstProducts.Location = new System.Drawing.Point(12, 37);
             this.lstProducts.Name = "lstProducts";
-            this.lstProducts.Size = new System.Drawing.Size(145, 225);
+            this.lstProducts.Size = new System.Drawing.Size(145, 264);
             this.lstProducts.TabIndex = 0;
             this.lstProducts.SelectedIndexChanged += new System.EventHandler(this.lstProducts_SelectedIndexChanged);
             // 
-            // tabControl1
+            // tabMain
             // 
-            this.tabControl1.Controls.Add(this.tabProducts);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(13, 13);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(662, 346);
-            this.tabControl1.TabIndex = 1;
+            this.tabMain.Controls.Add(this.tabProducts);
+            this.tabMain.Controls.Add(this.tabSuppliers);
+            this.tabMain.Controls.Add(this.tabCustomers);
+            this.tabMain.Controls.Add(this.tabOrders);
+            this.tabMain.Controls.Add(this.tabCreate);
+            this.tabMain.Controls.Add(this.tabReport);
+            this.tabMain.Location = new System.Drawing.Point(13, 13);
+            this.tabMain.Name = "tabMain";
+            this.tabMain.SelectedIndex = 0;
+            this.tabMain.Size = new System.Drawing.Size(662, 346);
+            this.tabMain.TabIndex = 1;
+            this.tabMain.SelectedIndexChanged += new System.EventHandler(this.tabMain_SelectedIndexChanged);
             // 
             // tabProducts
             // 
@@ -97,16 +110,16 @@ namespace ShopFinal
             // 
             // btnAddProd
             // 
-            this.btnAddProd.Location = new System.Drawing.Point(449, 263);
+            this.btnAddProd.Location = new System.Drawing.Point(318, 224);
             this.btnAddProd.Name = "btnAddProd";
-            this.btnAddProd.Size = new System.Drawing.Size(75, 20);
+            this.btnAddProd.Size = new System.Drawing.Size(75, 21);
             this.btnAddProd.TabIndex = 15;
             this.btnAddProd.Text = "Add";
             this.btnAddProd.UseVisualStyleBackColor = true;
             // 
             // btnRemoveProd
             // 
-            this.btnRemoveProd.Location = new System.Drawing.Point(530, 120);
+            this.btnRemoveProd.Location = new System.Drawing.Point(318, 122);
             this.btnRemoveProd.Name = "btnRemoveProd";
             this.btnRemoveProd.Size = new System.Drawing.Size(75, 20);
             this.btnRemoveProd.TabIndex = 14;
@@ -115,7 +128,7 @@ namespace ShopFinal
             // 
             // btnEditProd
             // 
-            this.btnEditProd.Location = new System.Drawing.Point(449, 120);
+            this.btnEditProd.Location = new System.Drawing.Point(318, 82);
             this.btnEditProd.Name = "btnEditProd";
             this.btnEditProd.Size = new System.Drawing.Size(75, 20);
             this.btnEditProd.TabIndex = 13;
@@ -126,7 +139,7 @@ namespace ShopFinal
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(210, 75);
+            this.label4.Location = new System.Drawing.Point(179, 37);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(49, 25);
             this.label4.TabIndex = 12;
@@ -136,7 +149,7 @@ namespace ShopFinal
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(210, 221);
+            this.label3.Location = new System.Drawing.Point(179, 183);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(95, 25);
             this.label3.TabIndex = 11;
@@ -145,7 +158,7 @@ namespace ShopFinal
             // lblAddProd
             // 
             this.lblAddProd.AutoSize = true;
-            this.lblAddProd.Location = new System.Drawing.Point(326, 246);
+            this.lblAddProd.Location = new System.Drawing.Point(181, 249);
             this.lblAddProd.Name = "lblAddProd";
             this.lblAddProd.Size = new System.Drawing.Size(76, 13);
             this.lblAddProd.TabIndex = 10;
@@ -154,7 +167,7 @@ namespace ShopFinal
             // lblAddSupp
             // 
             this.lblAddSupp.AutoSize = true;
-            this.lblAddSupp.Location = new System.Drawing.Point(212, 246);
+            this.lblAddSupp.Location = new System.Drawing.Point(181, 208);
             this.lblAddSupp.Name = "lblAddSupp";
             this.lblAddSupp.Size = new System.Drawing.Size(77, 13);
             this.lblAddSupp.TabIndex = 9;
@@ -164,14 +177,14 @@ namespace ShopFinal
             // 
             this.cboAddProd.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboAddProd.FormattingEnabled = true;
-            this.cboAddProd.Location = new System.Drawing.Point(215, 262);
+            this.cboAddProd.Location = new System.Drawing.Point(184, 224);
             this.cboAddProd.Name = "cboAddProd";
             this.cboAddProd.Size = new System.Drawing.Size(96, 21);
             this.cboAddProd.TabIndex = 8;
             // 
             // txtAddProd
             // 
-            this.txtAddProd.Location = new System.Drawing.Point(329, 263);
+            this.txtAddProd.Location = new System.Drawing.Point(184, 266);
             this.txtAddProd.Name = "txtAddProd";
             this.txtAddProd.Size = new System.Drawing.Size(100, 20);
             this.txtAddProd.TabIndex = 7;
@@ -179,7 +192,7 @@ namespace ShopFinal
             // lblEditProd
             // 
             this.lblEditProd.AutoSize = true;
-            this.lblEditProd.Location = new System.Drawing.Point(326, 103);
+            this.lblEditProd.Location = new System.Drawing.Point(181, 106);
             this.lblEditProd.Name = "lblEditProd";
             this.lblEditProd.Size = new System.Drawing.Size(76, 13);
             this.lblEditProd.TabIndex = 6;
@@ -188,7 +201,7 @@ namespace ShopFinal
             // lblEditSupp
             // 
             this.lblEditSupp.AutoSize = true;
-            this.lblEditSupp.Location = new System.Drawing.Point(215, 103);
+            this.lblEditSupp.Location = new System.Drawing.Point(181, 65);
             this.lblEditSupp.Name = "lblEditSupp";
             this.lblEditSupp.Size = new System.Drawing.Size(77, 13);
             this.lblEditSupp.TabIndex = 5;
@@ -198,14 +211,14 @@ namespace ShopFinal
             // 
             this.cboEditSupp.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboEditSupp.FormattingEnabled = true;
-            this.cboEditSupp.Location = new System.Drawing.Point(215, 119);
+            this.cboEditSupp.Location = new System.Drawing.Point(184, 81);
             this.cboEditSupp.Name = "cboEditSupp";
             this.cboEditSupp.Size = new System.Drawing.Size(96, 21);
             this.cboEditSupp.TabIndex = 4;
             // 
             // txtEditProd
             // 
-            this.txtEditProd.Location = new System.Drawing.Point(329, 120);
+            this.txtEditProd.Location = new System.Drawing.Point(184, 123);
             this.txtEditProd.Name = "txtEditProd";
             this.txtEditProd.Size = new System.Drawing.Size(100, 20);
             this.txtEditProd.TabIndex = 3;
@@ -214,33 +227,103 @@ namespace ShopFinal
             // 
             this.lblProdTitle.AutoSize = true;
             this.lblProdTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblProdTitle.Location = new System.Drawing.Point(14, 41);
+            this.lblProdTitle.Location = new System.Drawing.Point(6, 3);
             this.lblProdTitle.Name = "lblProdTitle";
             this.lblProdTitle.Size = new System.Drawing.Size(122, 31);
             this.lblProdTitle.TabIndex = 1;
             this.lblProdTitle.Text = "Products";
             // 
-            // tabPage2
+            // tabSuppliers
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(654, 320);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabSuppliers.Controls.Add(this.lstvProducts);
+            this.tabSuppliers.Controls.Add(this.txtSuppliers);
+            this.tabSuppliers.Controls.Add(this.lstSuppliers);
+            this.tabSuppliers.Location = new System.Drawing.Point(4, 22);
+            this.tabSuppliers.Name = "tabSuppliers";
+            this.tabSuppliers.Padding = new System.Windows.Forms.Padding(3);
+            this.tabSuppliers.Size = new System.Drawing.Size(654, 320);
+            this.tabSuppliers.TabIndex = 1;
+            this.tabSuppliers.Text = "Suppliers";
+            this.tabSuppliers.UseVisualStyleBackColor = true;
+            // 
+            // lstvProducts
+            // 
+            this.lstvProducts.HideSelection = false;
+            this.lstvProducts.Location = new System.Drawing.Point(166, 37);
+            this.lstvProducts.Name = "lstvProducts";
+            this.lstvProducts.Size = new System.Drawing.Size(473, 264);
+            this.lstvProducts.TabIndex = 4;
+            this.lstvProducts.UseCompatibleStateImageBehavior = false;
+            // 
+            // txtSuppliers
+            // 
+            this.txtSuppliers.AutoSize = true;
+            this.txtSuppliers.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSuppliers.Location = new System.Drawing.Point(0, 3);
+            this.txtSuppliers.Name = "txtSuppliers";
+            this.txtSuppliers.Size = new System.Drawing.Size(127, 31);
+            this.txtSuppliers.TabIndex = 3;
+            this.txtSuppliers.Text = "Supplires";
+            // 
+            // lstSuppliers
+            // 
+            this.lstSuppliers.FormattingEnabled = true;
+            this.lstSuppliers.Location = new System.Drawing.Point(6, 37);
+            this.lstSuppliers.Name = "lstSuppliers";
+            this.lstSuppliers.Size = new System.Drawing.Size(145, 264);
+            this.lstSuppliers.TabIndex = 2;
+            this.lstSuppliers.SelectedIndexChanged += new System.EventHandler(this.lstSuppliers_SelectedIndexChanged);
+            // 
+            // tabCustomers
+            // 
+            this.tabCustomers.Location = new System.Drawing.Point(4, 22);
+            this.tabCustomers.Name = "tabCustomers";
+            this.tabCustomers.Size = new System.Drawing.Size(654, 320);
+            this.tabCustomers.TabIndex = 2;
+            this.tabCustomers.Text = "Customers";
+            this.tabCustomers.UseVisualStyleBackColor = true;
+            // 
+            // tabOrders
+            // 
+            this.tabOrders.Location = new System.Drawing.Point(4, 22);
+            this.tabOrders.Name = "tabOrders";
+            this.tabOrders.Padding = new System.Windows.Forms.Padding(3);
+            this.tabOrders.Size = new System.Drawing.Size(654, 320);
+            this.tabOrders.TabIndex = 3;
+            this.tabOrders.Text = "Display Orders";
+            this.tabOrders.UseVisualStyleBackColor = true;
+            // 
+            // tabCreate
+            // 
+            this.tabCreate.Location = new System.Drawing.Point(4, 22);
+            this.tabCreate.Name = "tabCreate";
+            this.tabCreate.Size = new System.Drawing.Size(654, 320);
+            this.tabCreate.TabIndex = 4;
+            this.tabCreate.Text = "New Order";
+            this.tabCreate.UseVisualStyleBackColor = true;
+            // 
+            // tabReport
+            // 
+            this.tabReport.Location = new System.Drawing.Point(4, 22);
+            this.tabReport.Name = "tabReport";
+            this.tabReport.Size = new System.Drawing.Size(654, 320);
+            this.tabReport.TabIndex = 5;
+            this.tabReport.Text = "Reports";
+            this.tabReport.UseVisualStyleBackColor = true;
             // 
             // frmGeneral
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(687, 371);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.tabMain);
             this.Name = "frmGeneral";
             this.Text = "Shop Management";
-            this.tabControl1.ResumeLayout(false);
+            this.tabMain.ResumeLayout(false);
             this.tabProducts.ResumeLayout(false);
             this.tabProducts.PerformLayout();
+            this.tabSuppliers.ResumeLayout(false);
+            this.tabSuppliers.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -248,9 +331,9 @@ namespace ShopFinal
         #endregion
 
         private System.Windows.Forms.ListBox lstProducts;
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl tabMain;
         private System.Windows.Forms.TabPage tabProducts;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabSuppliers;
         private System.Windows.Forms.Label lblProdTitle;
         private System.Windows.Forms.ComboBox cboEditSupp;
         private System.Windows.Forms.TextBox txtEditProd;
@@ -265,6 +348,13 @@ namespace ShopFinal
         private System.Windows.Forms.Button btnEditProd;
         private System.Windows.Forms.Button btnAddProd;
         private System.Windows.Forms.Button btnRemoveProd;
+        private System.Windows.Forms.TabPage tabCustomers;
+        private System.Windows.Forms.ListView lstvProducts;
+        private System.Windows.Forms.Label txtSuppliers;
+        private System.Windows.Forms.ListBox lstSuppliers;
+        private System.Windows.Forms.TabPage tabOrders;
+        private System.Windows.Forms.TabPage tabCreate;
+        private System.Windows.Forms.TabPage tabReport;
     }
 }
 
--- ShopFinal/frmGeneral.cs
+++ ShopFinal/frmGeneral.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace ShopFinal
@@ -18,45 +19,99 @@ namespace ShopFinal
             DBSQL.Password = "secret";
             mySQL = DBSQL.Instance;
 
-            fillProducts();
-            fillSuppliers();
+
         }
 
 
-        private void fillProducts()
+        private void fillProducts(ListBox list, string key, string value)
         {
             products = mySQL.GetProductsData();
-            lstProducts.Items.Clear();
+            list.Items.Clear();
 
-            for (int i = 0; i < products.Length; i++)
-                lstProducts.Items.Add(products[i].Name);
-
+            foreach (Product product in products)
+                list.Items.Add(product);
+            list.ValueMember = value; // The value of the selected item(it's supplier id)
+            list.DisplayMember = key; // the value that will displayed at the list(product name)
         }
-
-        private void fillSuppliers()
+        private void fillSuppliersLst(ListBox list, string key, string value)
         {
             suppliers = mySQL.GetSuppliersData();
-            cboEditSupp.Items.Clear();
-            cboAddProd.Items.Clear();
+            list.Items.Clear();
+
+            for (int i = 0; i < suppliers.Length; i++)
+                list.Items.Add(suppliers[i]);
+            list.ValueMember = value; // The value of the selected item(it's supplier id)
+            list.DisplayMember = key; // the value that will displayed at the list(product name)
+        }
+
+        private void fillSuppliersCbo(ComboBox list, string key, string value)
+        {
+            //Dictionary cboSource = new Dictionary();
+            suppliers = mySQL.GetSuppliersData();
+            //cboEditSupp.Items.Clear();
+            list.Items.Clear();
 
             for (int i = 0; i < suppliers.Length; i++)
             {
-                cboEditSupp.Items.Add(suppliers[i].Name);
-                cboAddProd.Items.Add(suppliers[i].Name);
-
+                //cboEditSupp.Items.Add($"{suppliers[i].Name}({suppliers[i].Id}");
+                //cboEditSupp.Items.Add(suppliers[i]);
+                list.Items.Add(suppliers[i]);
             }
+            //cboEditSupp.DataSource = new BindingSource(cboSource, null);
+            list.DisplayMember = key;// the value that will displayed at the list(supplier name)
+            list.ValueMember = value; // the value of the selected item at the list(supplier id)
         }
-
 
 
         private void lstProducts_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Product p = products[lstProducts.SelectedIndex];
-            int i = Array.FindIndex(suppliers, supplier => supplier.Id == p.SupplierId);
-            Supplier s = suppliers[i];
-            cboEditSupp.SelectedIndex = cboEditSupp.Items.IndexOf(s.Name);
-            txtEditProd.Text = p.Name;
+            //Product p = products[lstProducts.SelectedIndex];
+            Product selectedProd = lstProducts.SelectedItem as Product;
+            //cboEditSupp.SelectedItem = ;
+            cboEditSupp.SelectedIndex = Array.FindIndex(suppliers, supplier => supplier.Id == selectedProd.SupplierId);
+            txtEditProd.Text = selectedProd.Name;
 
+
+
+
+        }
+
+
+        private void tabMain_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabMain.SelectedIndex == 0)
+            {
+                fillProducts(lstProducts, "Name", "SupplierId");
+                fillSuppliersCbo(cboAddProd, "Id", "Name");
+                fillSuppliersCbo(cboEditSupp, "Id", "Name");
+            }
+            else
+            if (tabMain.SelectedIndex == 1)
+            {
+                lstvProducts.Columns.Clear();
+                lstvProducts.Columns.Add("ID", 100);
+                lstvProducts.Columns.Add("Name", 100);
+                lstvProducts.Columns.Add("SupplierId", 100);
+                lstvProducts.View = View.Details;
+                lstvProducts.GridLines = true;
+                lstvProducts.FullRowSelect = true;
+                fillSuppliersLst(lstSuppliers, "Name", "Id");
+            }
+        }
+
+        private void lstSuppliers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Supplier selectedSupp = lstSuppliers.SelectedItem as Supplier;
+            lstvProducts.Items.Clear();
+            foreach (Product product in products)
+            {
+                if (product.SupplierId == selectedSupp.Id)
+                {
+                    string[] row = { product.Id.ToString(), product.Name, product.SupplierId.ToString() };
+                    ListViewItem itm = new ListViewItem(row);
+                    lstvProducts.Items.Add(itm);
+                }
+            }
         }
     }
 }
